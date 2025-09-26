4434c0a56635fa1bd16c2a4bd10b68a7f0e14060
shabad-hora <aashihora20@gmail.com>
2025-09-26 18:31:25 +0100

    Add files via upload

---
+++ summertime.py
@@ -0,0 +1,7 @@
+i=int(input("enter the temeperature:"))
+if (i < 23):
+    print("i is less than 23")
+    print("it is cold stay inside")
+else:
+    print("i is greater than 23")
+    print("go outside is is great")
